87536ff8aae4d87a1ac158bb621b42c09a490cc0
Ceulain <ceul1barth@hotmail.fr>
2025-01-05 23:35:29 +0100

    feat: add summary

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -40,11 +40,12 @@ import { Book } from "../../data/types";
 import "./inxde.css";
 
 //   }
-const Card = ({ author, title }: Book) => {
+const Card = ({ author, summary, title }: Book) => {
   return (
     <article>
       <h3>{title}</h3>
       <p>{author}</p>
+      <p>{summary}</p>
     </article>
   );
 };
